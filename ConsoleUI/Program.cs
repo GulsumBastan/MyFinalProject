c69004136af6f49505aa2805056f1fa942f02244
Gülsüm Baştan <102551681+GulsumBastan@users.noreply.github.com>
2022-09-11 22:05:37 +0300

    Kategori,sipariş düzenlemeleri.

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -10,16 +10,25 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
+            // ProductTest();
+            //IoC
+            CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+            foreach(var category in categoryManager.GetAll())
+            {
+                Console.WriteLine(category.CategoryName);
+            }
+
+        }
+
+        private static void ProductTest()
+        {
             ProductManager productManager = new ProductManager(new EfProductDal());
 
-            foreach (var product in productManager.GetAll())
+            foreach (var product in productManager.GetByUnitPrice(40, 100))
             {
                 Console.WriteLine(product.ProductName);
             }
-
-
         }
 
-
-        }
+    }
 }
