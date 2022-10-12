e7bd1f47d2d1443a9c0a97050c8c86468d94a7f2
Gülsüm Baştan <102551681+GulsumBastan@users.noreply.github.com>
2022-10-12 17:50:44 +0300

    Transsaction,Performance aspect

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,6 +1,9 @@
 using Autofac;
 using Autofac.Extensions.DependencyInjection;
 using Business.DependencyResolvers.Autofac;
+using Core.DepenencyResolvers;
+using Core.Extensions;
+using Core.Utilities.IoC;
 using Core.Utilities.Security.Encryption;
 using Core.Utilities.Security.JWT;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
@@ -37,6 +40,10 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
             IssuerSigningKey = SecurityKeyHelper.CreateSecurityKey(tokenOptions.SecurityKey)
         };
     });
+
+builder.Services.AddDependencyResolvers(new ICoreModule[] {
+    new CoreModule() });
+
 
 
 
