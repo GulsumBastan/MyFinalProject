ec7ba84532aaf15a845278c24fbe12bd6babe47b
Gülsüm Baştan <102551681+GulsumBastan@users.noreply.github.com>
2022-10-21 21:10:00 +0300

    update

--- WebAPI/Controllers/ProductsController.cs
+++ WebAPI/Controllers/ProductsController.cs
@@ -1,8 +1,5 @@
 ﻿using Business.Abstract;
-using Business.Concrete;
-using DataAccess.Concrete.EntityFramework;
 using Entities.Concrete;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace WebAPI.Controllers
